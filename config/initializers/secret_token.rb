f88f53b1364470aa58765f8465c6c41a2e709017
Andrew Aarestad <andrew@etal-andothers.com>
2013-02-07 18:15:44 -0800

    initial commit of skeleton app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WalruServer::Application.config.secret_token = '8105fafc94c2ea133c20edf78fe93973bde0c38de7548337a85e7105a49e416fef6cd500a23c5146618874b08ddd0fc70f75796f5c70b5407e20d56f6c6de896'
